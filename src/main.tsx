9d87a4ed39bcdb0be0bda95e3cf132427a99d394
MatheusVict <matheusvictorhenrique@gmail.com>
2023-12-17 19:10:39 -0300

    feat: react router dom config
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Layout } from "./components/Layout/index.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Layout>
-      <App />
+      <RouterProvider router={router} />
     </Layout>
   </React.StrictMode>
 );
